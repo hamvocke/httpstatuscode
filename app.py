f56d04dc7f9b03a22bdb2394d22692f380bf3a24
Ham Vocke <hermann.vocke@gmail.com>
2015-12-17 20:06:36 +0100

    creating basic statuscode endpoint

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'Welcome to httpstatusco.de'
+
+@app.route('/<int:statuscode>')
+def statuscode(statuscode):
+    return 'Your statuscode is %d' % statuscode
 
 if __name__ == '__main__':
     app.run()
