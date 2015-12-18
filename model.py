5fe1c54c2341ab64cdf354c3b36059ec8244ed4b
Ham Vocke <hermann.vocke@gmail.com>
2015-12-18 11:37:51 +0100

    adding title to statuscodes

--- model.py
+++ model.py
@@ -1,10 +1,11 @@
 class Statuscode:
-    def __init__(self, code, description):
+    def __init__(self, code, title, description):
         self.code = code
+        self.title = title
         self.description = description
 
 statuscodes = {
-    200: Statuscode(200, "OK"),
-    201: Statuscode(201, "ACCEPTED"),
-    404: Statuscode(404, "NOT FOUND")
+    200: Statuscode(200, "OK", "Everything was alright"),
+    201: Statuscode(201, "ACCEPTED", "The request was accepted but is still processing"),
+    404: Statuscode(404, "NOT FOUND", "The requested resource could not be found")
 }
