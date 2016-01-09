0d7e6f86ebee1f2d06e447b05ab26a555a1986c3
Ham Vocke <hermann.vocke@gmail.com>
2016-01-09 15:27:43 +0100

    adding search to index

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort, render_template
+from flask import Flask, abort, render_template, request, flash, redirect, url_for
 import model
 
 app = Flask(__name__)
@@ -7,12 +7,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/<int:statuscode>')
+@app.route('/<string:statuscode>')
 def statuscode(statuscode):
     if statuscode not in model.statuscodes:
         abort(404)
     status = model.statuscodes[statuscode]
     return render_template('statuscode.html', status=status)
+
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form['query']
+    if not query:
+        flash('Please provide a query') # TODO: need to set an API secret key for this to work. Consider setting one using env vars, deploy that with the application
+        return render_template('index.html')
+    elif query not in model.statuscodes:
+        flash("Sorry, I don't know the statuscode %s" % query)
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('statuscode', statuscode=query))
+
 
 @app.errorhandler(404)
 def statuscode_not_found(error):
--- test.py
+++ test.py
@@ -6,20 +6,39 @@ class StatusCodeTestCase(unittest.TestCase):
         self.app = app.app.test_client()
 
     def test_should_return_200_for_known_status_code(self):
-        response = self.app.get('/404');
+        response = self.app.get('/404')
         self.assertEqual(response.status_code, 200)
         self.assertTrue(b"404" in response.data)
         self.assertTrue(b"Not Found" in response.data)
 
     def test_should_return_404_for_unknown_status_code(self):
-        response = self.app.get('/800');
+        response = self.app.get('/800')
         self.assertEqual(response.status_code, 404)
         self.assertTrue(b"Sorry, I couldn't find that statuscode" in response.data)
 
     def test_should_return_landing_page(self):
-        response = self.app.get('/');
+        response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTrue(b'Find out what your HTTP statuscode stands for' in response.data)
+
+    def test_should_redirect_to_existing_page(self):
+        respone = self.search_without_redirect("415")
+        self.assertEqual(respone.status_code, 302)
+
+    def test_should_find_existing_page(self):
+        respone = self.search("415")
+        self.assertEqual(respone.status_code, 200)
+
+    def test_should_return_to_index_if_searched_page_cannot_be_found(self):
+        respone = self.search_without_redirect("499")
+        # self.assertEqual(respone.status_code, 302)
+        assert "Sorry, I don't know the statuscode 499" in response.data
+
+    def search_without_redirect(self, query):
+        return self.app.post('/search', data=dict(query=query), follow_redirects=False)
+
+    def search(self, query):
+        return self.app.post('/search', data=dict(query=query), follow_redirects=True)
 
 if __name__ == '__main__':
     unittest.main()
